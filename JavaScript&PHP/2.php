42beac424723efaa17b240689a778bcdefcfd2d0
hwawon <ilove4622@gmail.com>
2022-09-13 16:36:49 +0900

    9/5(月)~9/13(火) WEB/HTML/CSS/JavaScript/PHP 1. html basics study 2. web&server basics study 3. web server for chrome using 4. apache web server(MAMP) using 5. web server basics study + 6. CSS basics study 7. JavaScript&PHP basics study

---
+++ JavaScript&PHP/2.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>JavaScript</title>
+    <link rel="stylesheet" href="style.css" type="text/css">
+  </head>
+  <body>
+    <h1>JavaScript</h1>
+    <script>
+      document.write(10+10);
+      document.write(10+"10");
+    </script>
+    <?php
+      echo 10+10;
+      echo "10"+"10";
+     ?>
+  </body>
+</html>
---
+++ JavaScript&PHP/PHPJS/1.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>JavaScript</title>
+    <link rel="stylesheet" href="style.css" type="text/css">
+  </head>
+  <body>
+    <h1>PHP</h1>
+      <?php
+        echo 1+10;
+       ?>
+
+    <h1>JavaScript</h1>
+    <script>
+      document.write(10+2);
+    </script>
+  </body>
+</html>
