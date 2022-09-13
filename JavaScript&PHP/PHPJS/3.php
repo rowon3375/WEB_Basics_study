bc82173e1f1c33d0a7379444e000a14e94106854
hwawon <ilove4622@gmail.com>
2022-09-13 17:16:34 +0900

    9/5(月)~9/13(火) WEB/HTML/CSS/JavaScript/PHP 1. html basics study 2. web&server basics study 3. web server for chrome using 4. apache web server(MAMP) using 5. web server basics study + 6. CSS basics study 7. JavaScript&PHP basics study

---
+++ JavaScript&PHP/PHPJS/3.php
@@ -0,0 +1,23 @@
+<?php
+  $name = "이고잉";
+?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>JavaScript</title>
+    <link rel="stylesheet" href="style.css" type="text/css">
+  </head>
+  <body>
+    <h1>JavaScript</h1>
+    <script charset="utf-8">
+      name = "이고잉";
+      document.write("안녕하세요"+name);
+    </script>
+
+    <h1>PHP</h1>
+    <?php
+      echo "안녕하세요".$name;
+     ?>
+  </body>
+</html>
