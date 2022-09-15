787f5b4f438f174fd1ecbaed5500e0091cb68895
hwawon <ilove4622@gmail.com>
2022-09-15 15:21:09 +0900

    9/5(月)~9/15(木) WEB/HTML/CSS/JavaScript/PHP 1. html basics study 2. web&server basics study 3. web server for chrome using 4. apache web server(MAMP) using 5. web server basics study 6. CSS basics study 7. JavaScript&PHP basics study + 8. Array, if, while basics, function

---
+++ JavaScript&PHP/PHPJS/11.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Function</title>
+  </head>
+
+  <body>
+    <h1>Javascript</h1>
+    <script type="text/javascript">
+      function a(){
+          document.write("hello function");
+      }
+      a();
+
+    </script>
+
+
+    <h1>PHP</h1>
+    <?php
+      function b(){
+        echo "hello php";
+      }
+
+      b();
+     ?>
+
+
+  </body>
+</html>
---
+++ JavaScript&PHP/PHPJS/12.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Function</title>
+  </head>
+
+  <body>
+    <h1>Javascript</h1>
+    <script type="text/javascript">
+      function a(input){
+          return input+1;
+      }
+      document.write(a(1));
+
+    </script>
+
+
+    <h1>PHP</h1>
+    <?php
+      function b(){
+        echo "hello php";
+      }
+
+      b();
+     ?>
+
+
+  </body>
+</html>
