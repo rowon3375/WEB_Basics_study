55bbcba847d3875e93040b91d9c70d2f2359c54f
hwawon <ilove4622@gmail.com>
2022-09-22 19:58:41 +0900

    9/5(月)~9/22(木) WEB/HTML/CSS/JavaScript/PHP/MySQL(MariaDB) 1. html basics study 2. web&server basics study 3. web server for chrome using 4. apache web server(MAMP) using 5. web server basics study 6. CSS basics study 7. JavaScript&PHP basics study 8. Array, if, while basics, function 9. MySQL basics study(MariaDB Server using) 10. PHP&DB connecting 11. form 12. security&database security 13. Library(twitter bootstrap) 14. Review

--- Review/index.php
+++ Review/index.php
@@ -2,7 +2,7 @@
 <?php
   $conn = mysqli_connect("localhost", "root", "337575");
   mysqli_select_db($conn, 'opentutorials2');
-  // mysql에다가 실제로 정보를 조회, 수정, 삭제 할 때 사용함.
+  // mysqli_query: mysql에다가 실제로 정보를 조회, 수정, 삭제 할 때 사용함.
   $sql = "SELECT * FROM `topic`";
   $result = mysqli_query($conn, $sql);
 ?>
@@ -27,7 +27,7 @@
   <body id="body">
     <!-- 헤더 -->
     <header>
-      <h1>WEB Basic</h1>
+      <a href="index.php"><h1>WEB Basic</h1></a>
     </header>
 
     <!-- 네비게이션 -->
@@ -46,26 +46,30 @@
     <div class="content">
         <article>
             <?php
-            // mysqli_fetch_assoc: php가 사용할 수 있도록 가공해 주는 함수
-            // var_dump(): 데이터 값을 정확하게 설명해준다.
-            // null =  false 이다.
-              $id = mysqli_real_escape_string($conn, $_GET['id']);
-            //mysqli_real_escape_string: 외부로부터 들어오는 정보는 중요해서 데이터 베이스에 저장 전에 이걸로 저장한다.
-              $sql = "select topic.id, topic.title, topic.description, user.name,
-              topic.created from topic left join user on topic.author = user.id
-              where topic.id=".$id;
-              $result = mysqli_query($conn, $sql);
-              $row = mysqli_fetch_assoc($result);
-            ?>
-
-            <h2><?= htmlspecialchars($row['title']); ?></h2>
-            <div><?= htmlspecialchars($row['created']) ?> | <?= htmlspecialchars($row['name']) ?></div>
-            <div><?= htmlspecialchars($row['description']) ?></div>
+            if(empty($_GET['id'])){
+              echo "hi";
+            } else{
+              // mysqli_fetch_assoc: php가 사용할 수 있도록 가공해 주는 함수
+              // var_dump(): 데이터 값을 정확하게 설명해준다.
+              // null =  false 이다.
+                $id = mysqli_real_escape_string($conn, $_GET['id']);
+              //mysqli_real_escape_string: 외부로부터 들어오는 정보는 중요해서 데이터 베이스에 저장 전에 이걸로 저장한다.
+                $sql = "select topic.id, topic.title, topic.description, user.name,
+                topic.created from topic left join user on topic.author = user.id
+                where topic.id=".$id;
+                $result = mysqli_query($conn, $sql);
+                $row = mysqli_fetch_assoc($result);
+                ?>
+                <h2><?= htmlspecialchars($row['title']); ?></h2>
+                <div><?= htmlspecialchars($row['created']) ?> | <?= htmlspecialchars($row['name']) ?></div>
+                <div><?= htmlspecialchars($row['description']) ?></div>
+            <?php } ?>
         </article>
 
         <!-- 버튼 -->
         <input type="button" name="name" value="White" onclick="white()">
         <input type="button" name="name" value="Black" onclick="black()">
+        <a href="write.php">Write</a>
 
       </div>
 
--- Review/process.php
+++ Review/process.php
@@ -1,4 +1,36 @@
 <?php
   // print_r($_POST);
+  // 순서
+  // 1. 데이터베이스 접속
+    $conn = mysqli_connect("localhost", "root", "337575");
+    mysqli_select_db($conn, 'opentutorials2');
 
+  // 2. 저자가 user 테이블에 존재하는지 여부 체크
+  $author = mysqli_real_escape_string($conn, $_POST['author']);
+  $sql = "SELECT * FROM `user` WHERE `name` = '{$author}'";
+  $result = mysqli_query($conn, $sql);
+      //var_dump($result-> num_rows);
+  if($result -> num_rows > 0){
+    // 사용자가 존재한다면---> 3. 존재한다면 해당 user의 id를 알아낸다.
+    // $user_id는 $row의 정보 중, id 값만 사용한다.
+    $row = mysqli_fetch_assoc($result);
+    $user_id = $row['id'];
+
+  } else{
+    // 사용자가 존재하지 않는다면 ---> 4. 존재하지 않는다면 저자를 user에 추가 후 id를 알아낸다.
+    $sql = "insert into user (id, name) values (null, '{$author}')";
+    $result = mysqli_query($conn, $sql);
+    //id 값을 바탕으로 해서 author의 user id값을 부여한다.
+    //mysqli_insert_id: 해당 함수가 실행되기 직전에 실행된 sql 쿼리의 결과의 행의 id값을 리턴함.
+    $user_id = mysqli_insert_id($conn);
+  }
+    // 5. 사용자가 입력한 제목, 저자, 본문 등을 topic 테이블에 추가
+  $title = mysqli_real_escape_string($conn, $_POST['title']);
+  $description = mysqli_real_escape_string($conn, $_POST['description']);
+  $sql = "INSERT INTO `topic` (`id`, `title`, `description`, `author`, `created`)
+          VALUES (NULL, '{$title}' , '{$description}', '{$user_id}', now())";
+
+  mysqli_query($conn, $sql);
+  //등록이 왼료되면 해당 사이트로 다시 이동시킨다.
+  header('Location: index.php');
 ?>
