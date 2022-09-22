10adba38367b861ecbd2df7a8cfda43f83c8a40f
hwawon <ilove4622@gmail.com>
2022-09-22 18:38:11 +0900

    9/5(月)~9/22(木) WEB/HTML/CSS/JavaScript/PHP/MySQL(MariaDB) 1. html basics study 2. web&server basics study 3. web server for chrome using 4. apache web server(MAMP) using 5. web server basics study 6. CSS basics study 7. JavaScript&PHP basics study 8. Array, if, while basics, function 9. MySQL basics study(MariaDB Server using) 10. PHP&DB connecting 11. form 12. security&database security 13. Library(twitter bootstrap) 14. Review

--- Review/index.php
+++ Review/index.php
@@ -11,8 +11,20 @@
   <head>
     <meta charset="utf-8">
     <title>Review</title>
+    <link rel="stylesheet" href="style.css" type="text/css">
+    <script type="text/javascript">
+      function black(){
+        document.getElementById('body').className="black";
+      }
+      function white(){
+        document.getElementById('body').className="";
+      }
+
+    </script>
   </head>
-  <body>
+
+
+  <body id="body">
     <!-- 헤더 -->
     <header>
       <h1>WEB Basic</h1>
@@ -31,23 +43,31 @@
     </nav>
 
     <!-- 본문 -->
-    <article>
-      <?php
-      // mysqli_fetch_assoc: php가 사용할 수 있도록 가공해 주는 함수
-      // var_dump(): 데이터 값을 정확하게 설명해준다.
-      // null =  false 이다.
-        $id = mysqli_real_escape_string($conn, $_GET['id']);
-      //mysqli_real_escape_string: 외부로부터 들어오는 정보는 중요해서 데이터 베이스에 저장 전에 이걸로 저장한다.
-        $sql = "select topic.id, topic.title, topic.description, user.name,
-        topic.created from topic left join user on topic.author = user.id
-        where topic.id=".$id;
-        $result = mysqli_query($conn, $sql);
-        $row = mysqli_fetch_assoc($result);
-      ?>
+    <div class="content">
+        <article>
+            <?php
+            // mysqli_fetch_assoc: php가 사용할 수 있도록 가공해 주는 함수
+            // var_dump(): 데이터 값을 정확하게 설명해준다.
+            // null =  false 이다.
+              $id = mysqli_real_escape_string($conn, $_GET['id']);
+            //mysqli_real_escape_string: 외부로부터 들어오는 정보는 중요해서 데이터 베이스에 저장 전에 이걸로 저장한다.
+              $sql = "select topic.id, topic.title, topic.description, user.name,
+              topic.created from topic left join user on topic.author = user.id
+              where topic.id=".$id;
+              $result = mysqli_query($conn, $sql);
+              $row = mysqli_fetch_assoc($result);
+            ?>
 
-      <h2><?= htmlspecialchars($row['title']); ?></h2>
-      <div><?= htmlspecialchars($row['created']) ?> | <?= htmlspecialchars($row['name']) ?></div>
-      <div><?= htmlspecialchars($row['description']) ?></div>
-    </article>
+            <h2><?= htmlspecialchars($row['title']); ?></h2>
+            <div><?= htmlspecialchars($row['created']) ?> | <?= htmlspecialchars($row['name']) ?></div>
+            <div><?= htmlspecialchars($row['description']) ?></div>
+        </article>
+
+        <!-- 버튼 -->
+        <input type="button" name="name" value="White" onclick="white()">
+        <input type="button" name="name" value="Black" onclick="black()">
+
+      </div>
+
   </body>
 </html>
